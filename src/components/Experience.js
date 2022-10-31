1e4de7510989c9f7769a904bb6cf8c99a9917342
yralaral021 <yralaral021@student.wethinkcode.co.za>
2022-10-31 14:37:58 +0200

    code refactoring

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,33 +1,19 @@
-import React from 'react'
+import React from 'react';
 import myPic from '../resources/yeyethu.png';
 import '../css/StylingStandard.css';
-import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom';
+import StandardLayout from './StandardLayout.js';
 
 
 function Experience(){
     return(
         <>
-        <div className="standardSection">
-            
-            <div className="introBox">
-                <h1>Yeyethu Ralarala</h1>
-                <p>EXPERIENCE PAGE Lorem ipsum dolor sit amet, consectetur jjfjfjajlalalallaadip kdkjhdh
-                    jsjhghala jfjjjfjjaj kkfjajajja kkaklrefeajroieij testing lkksjdjdjdjd 
-                    jdjdjjdjdjdjdjd kjdjjdjdjdj oiehhsjahah akjjdjjsdjdsjsj.
-
-                </p>
-                <div className="readMoreBox">
-                    <div className="read">
-                        <NavLink exact to='/careerexpectation' className="readMore">Read More</NavLink>
-                    </div>                    
-                </div>
-            </div> 
-
-            <div className="myPictureContainer">
-            <img src={myPic} alt="Yeyethu's image" className="picture"/>
-            </div>  
-
-        </div> 
+        <StandardLayout
+        title = "Experience"
+        body = ""
+        img = {myPic}
+        readMore = "Read more"
+        />
         </>   
     );    
 }
--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -3,29 +3,29 @@ import {NavLink} from 'react-router-dom'
 import '../css/StylingHeader.css';
 
 
-function PageHeader(){
+function PageHeader(){  
     return(
         <>
     <header>    
         <div className="pageHeader page-header">
 
             <nav>
-            <div className="homeAboutContainer">
-                <NavLink exact to='/home' className="container" 
-                activeClassName="activeContainer">Home</NavLink>
+                <div className="homeAboutContainer">
+                    <NavLink exact to='/home' className={"container"} 
+                    activeClassName="activeContainer">Home</NavLink>
 
-                <NavLink to='/about' className="container" 
-                activeClassName="activeContainer">About</NavLink>
+                    <NavLink to='/about' className={"container"} 
+                    activeClassName="activeContainer">About</NavLink>
 
-                <NavLink to='/experience' className="container" 
-                activeClassName="activeContainer">Experience</NavLink>
+                    <NavLink to='/experience' className={"container"} 
+                    activeClassName="activeContainer">Experience</NavLink>
 
-                <NavLink to='/careerexpectation' className="container" 
-                activeClassName="activeContainer">Career Expectation</NavLink>
+                    <NavLink to='/careerexpectation' className={"container"} 
+                    activeClassName="activeContainer">Career Expectation</NavLink>
 
-                <NavLink to='/hobbies' className="container" 
-                activeClassName="activeContainer">Hobbies</NavLink>
-            </div> 
+                    <NavLink to='/hobbies' className={"container"} 
+                    activeClassName="activeContainer">Hobbies</NavLink>
+                </div> 
             </nav>           
             
         </div>
